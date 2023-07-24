26466221adfb7e8ac2269ca2c815b5a41af5db99
Henry Lee <henryleeworld@gmail.com>
2023-07-25 00:19:42 +0800

    feat:升級至 laravel 10

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -3,11 +3,14 @@
 namespace Tests\Unit\Models;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testPasswordWillBeHashed()
     {
         Hash::shouldReceive("make")
